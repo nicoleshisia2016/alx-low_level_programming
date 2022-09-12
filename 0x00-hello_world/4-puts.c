7f78774d5e55b14a836ce65fa36b65aef57ba204
nicole <nicoleshisia2016@gmail.com>
2022-09-12 03:31:35 -0700

    coding

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+* main - print the string in the put function
+*
+Description: using the main function
+*Description: using the main function
+*this program prints "programming is like building a multillingual puzzle
+*Return: 0
+*/
+int main(void)
+{
+        puts(*\"Programming is like building a multillingual puzzle");
+        return (0);
+}
