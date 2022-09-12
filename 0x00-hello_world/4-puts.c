8c55324a364df11444bfab7d886845a6c58087f6
nicole <nicoleshisia2016@gmail.com>
2022-09-12 03:54:40 -0700

    Code

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,13 +3,12 @@
 /**
 * main - print the string in the put function
 *
-Description: using the main function
-*Description: using the main function
-*this program prints "programming is like building a multillingual puzzle
+* Description: using the main function
+* this program prints "Programming is like building a multilingual puzzle
 *Return: 0
 */
 int main(void)
 {
-        puts(*\"Programming is like building a multillingual puzzle");
+        puts("\"Programming is like building a multilingual puzzle");
         return (0);
 }
